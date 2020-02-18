dab9af7495a18b1675dc1d1b0cd2e6b9eaa42acd
Andrew Novykov <papapin777@gmail.com>
2020-02-17 21:11:28 -0800

    S4E21 Class Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React , {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      string: 'Hello Andrew'
+    }
+  }
+
+
+  render() {
+   return (
+     <div>
+        <p>{this.state.string}</p>
+        <p>{this.state.string}</p>
+        <button onClick={()=> this.setState({string: 'Huylo'})}  >change text</button>
+     </div>
+    
+   )
+  }
 }
 
 export default App;
